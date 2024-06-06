484189caaba2382062786c536f3194098084b463
isratkakon <isratkakon0@gmail.com>
2024-06-06 09:49:29 +0600

    ui-created

---
+++ vue-project/src/components/students/Add.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { reactive } from "vue";
+const formData = reactive({
+  stuname:"",
+  email:"",
+});
+const handleAddStudentForm = async () => {
+  console.log("Form Submitted", formData);
+};
+</script>
+
+<template>
+  <div class="shadow-md pb-6">
+    <div class="bg-indigo-600 p-4">
+      <h1 class="text-3xl font-bold text-center text-white">Add Student</h1>
+    </div>
+
+    <form 
+    @submit.prevent="handleAddStudentForm"
+    class="w-full"
+    id="AddStudentForm"
+    >
+      <div class="flex items-center m-6">
+        <div class="w-1/5">
+          <label class="font-medium" for="stuname"> Name: </label>
+        </div>
+        <div class="w-4/5">
+          <input 
+            type="text"
+            id="stuname"
+            v-model.trim="formData.stuname"
+            class="border-2 border-gray-200 w-full px-4 py-2"
+            placeholder="Write your name"
+            required
+          >
+        </div>
+      </div>
+      <div class="flex items-center m-6">
+        <div class="w-1/5">
+          <label class="font-medium" for="email"> Email: </label>
+        </div>
+        <div class="w-4/5">
+          <input 
+            type="email"
+            id="email"
+            v-model.trim="formData.email"
+            class="border-2 border-gray-200 w-full px-4 py-2"
+            placeholder="Write your email"
+            required
+          >
+        </div>
+      </div>
+
+      <div class="m-8 flex justify-center">
+        <button 
+        type="submit"
+        class="bg-purple-700 text-white font-medium py-2 rounded-md px-6 hover:bg-purple-800 mr-6"
+        >
+        Add
+      </button>
+
+      <button 
+        type="submit"
+        class="bg-emerald-700 text-white font-medium p-2 rounded-md hover:bg-emerald-800"
+        >
+        Back to Home
+      </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory} from "vue-router";
+import List from '../components/students/List.vue';
+import Add from '../components/students/Add.vue';
+import View from '../components/students/View.vue';
+import Edit from '../components/students/Edit.vue';
+import NotFound from '../views/NotFound.vue'
+
+const routes = [
+    {
+        path: '/',
+        name:'list',
+        component: List
+    },
+    {
+        path: '/add',
+        name: 'add',
+        component: Add
+    },
+    {
+        path: '/view/:id',
+        name: 'view',
+        component: View
+    },
+    {
+        path: '/edit/:id',
+        name: 'edit',
+        component: Edit
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes
+})
+
+export default router
